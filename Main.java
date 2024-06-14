7e6d001ad83badaa14e20ac1c27ab86b7cb12d8c
s31233 <162576869+s31233@users.noreply.github.com>
2024-06-14 20:58:48 +0200

    TASK12
    TASK12

--- Main.java
+++ Main.java
@@ -1,71 +1,172 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.*;
+import java.util.List;
+import java.util.Random;
+import java.awt.Color;
+import java.awt.Graphics;
+
 
 public class Main {
     public static void main(String[] args) {
-        //Task 1
-        ArrayList<Car> cars = new ArrayList<>();
-        cars.add(new Car("Toyota", 2015));
-        cars.add(new Car("Ford", 2014));
-        cars.add(new Car("Kia", 2050));
-        cars.add(new Car("BMW", 2018));
-        cars.add(new Car("Audi", 2016));
-        cars.add(new Car("Mercedes", 2017));
-        cars.add(new Car("Honda", 2013));
-        cars.add(new Car("Hyundai", 2012));
-        cars.add(new Car("Mazda", 2019));
-        cars.add(new Car("Chevrolet", 2011));
+        DrawingApp app = new DrawingApp();
+        app.setVisible(true);
+    }
+}
+//Enum ShapeType
+ enum ShapeType {
+    RECTANGLE,
+    CIRCLE
+}
+//Class ShapeFactory
 
-        Collections.sort(cars);
+    class ShapeFactory {
+    private ShapeType shapeType;
+    private Random random;
 
-        for (Car car : cars){
-            System.out.println(car);
-        }
+    public ShapeFactory() {
+        this.shapeType = ShapeType.RECTANGLE;
+        this.random = new Random();
+    }
 
-        //Task 2
-        //Add 3 more items and implement "Item" class
-        ArrayList<Item> items = new ArrayList<>();
-        items.add(new Item(1,"Pen"));
-        items.add(new Item(2,"Eraser"));
-        items.add(new Item(3, "Notebook"));
-        items.add(new Item(4, "Pencil"));
-        items.add(new Item(5, "Ruler"));
+    public void setShapeType(ShapeType shapeType) {
+        this.shapeType = shapeType;
+    }
 
-        for (Item item : items) {
-            item.show();
-        }
-        //HashMap
-        HashMap<Integer, String> itemMap = new HashMap<>();
-        for (Item item : items){
-            itemMap.put(item.getId(), item.getName());
-        }
+    public Shape createShape(int x, int y, int width, int height) {
+        Color color = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
 
-        for (Map.Entry<Integer,String> entry : itemMap.entrySet()){
-            System.out.println("ID: " + entry.getKey() + ", Name: " + entry.getValue());
-        }
-
-        //Task 3
-        ArrayList<Item> itemsTask3 = new ArrayList<>();
-        for (int i = 6; i <=15; i++){
-            items.add(new Item(i,"Item" + i));
-        }
-        List<Item> sublist = items.subList(0,5);
-        HashSet<Item> itemSet = new HashSet<>(sublist);
-
-        for (Item item : itemSet){
-            System.out.println(item);
-        }
-        //Task 4
-        Task4 task4 = new Task4();
-        System.out.println("Enter 10 numbers");
-        task4.fillArray();
-
-        System.out.println("Contents of array: ");
-        for (int number : task4.numbers){
-            System.out.println(number);
+        switch (shapeType) {
+            case RECTANGLE:
+                return new RectangleShape(x, y, width, height, color);
+            case CIRCLE:
+                int diameter = Math.min(width, height);
+                return new CircleShape(x, y, diameter, color);
+            default:
+                return null;
         }
     }
+}
+// Class DrawingCanvas
+
+    class DrawingCanvas extends JPanel {
+    private List<Shape> shapes;
+    private ShapeFactory shapeFactory;
+
+    public DrawingCanvas(ShapeFactory shapeFactory) {
+        this.shapes = new ArrayList<>();
+        this.shapeFactory = shapeFactory;
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int width = Integer.parseInt(JOptionPane.showInputDialog("Enter width:"));
+                int height = Integer.parseInt(JOptionPane.showInputDialog("Enter height:"));
+                Shape shape = shapeFactory.createShape(e.getX(), e.getY(), width, height);
+                shapes.add(shape);
+                repaint();
+            }
+        });
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (Shape shape : shapes) {
+            shape.draw(g);
+        }
+    }
+}
+//Abstract class Shape
+    abstract class Shape {
+    protected int x, y;
+    protected Color color;
+
+    public Shape(int x, int y, Color color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public abstract void draw(Graphics g);
+}
+// Class RectangleShape
+    class RectangleShape extends Shape {
+    private int width, height;
+
+    public RectangleShape(int x, int y, int width, int height, Color color) {
+        super(x, y, color);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillRect(x, y, width, height);
+    }
+}
+// Class CircleShape
+    class CircleShape extends Shape {
+    private int diameter;
+
+    public CircleShape(int x, int y, int diameter, Color color) {
+        super(x, y, color);
+        this.diameter = diameter;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillOval(x, y, diameter, diameter);
+    }
+}
+// Class DrawingApp
+    class DrawingApp extends JFrame {
+    private DrawingCanvas canvas;
+    private ShapeFactory shapeFactory;
+
+    public DrawingApp() {
+        setTitle("Simple Drawing Application");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        shapeFactory = new ShapeFactory();
+        canvas = new DrawingCanvas(shapeFactory);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu shapeMenu = new JMenu("Shape");
+
+        JMenuItem rectangleItem = new JMenuItem("Rectangle");
+        rectangleItem.addActionListener(new ShapeMenuActionListener(ShapeType.RECTANGLE));
+
+        JMenuItem circleItem = new JMenuItem("Circle");
+        circleItem.addActionListener(new ShapeMenuActionListener(ShapeType.CIRCLE));
+
+        shapeMenu.add(rectangleItem);
+        shapeMenu.add(circleItem);
+        menuBar.add(shapeMenu);
+
+        setJMenuBar(menuBar);
+        add(canvas, BorderLayout.CENTER);
+    }
+
+    private class ShapeMenuActionListener implements ActionListener {
+        private ShapeType shapeType;
+
+        public ShapeMenuActionListener(ShapeType shapeType) {
+            this.shapeType = shapeType;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            shapeFactory.setShapeType(shapeType);
+        }
+    }
+
+
 }
