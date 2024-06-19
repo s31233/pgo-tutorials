5b3f2b6eccbfe4463665864922107d839b8c3c40
s31233 <162576869+s31233@users.noreply.github.com>
2024-06-19 21:01:14 +0200

    TASK10
    TASK10

--- Person.java
+++ Person.java
@@ -1,31 +1,5 @@
 public class Person {
+    public String name;
+    public int age;
 
-    private int age;
-    private boolean isWarsaw;
-
-
-    public Person(int age, boolean isWarsaw) {
-
-        this.age = age;
-        this.isWarsaw = isWarsaw;
-    }
-
-    // Gettery i settery
-
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public boolean isWarsaw() {
-        return isWarsaw;
-    }
-
-    public void setWarsaw(boolean warsaw) {
-        isWarsaw = warsaw;
-    }
 }
