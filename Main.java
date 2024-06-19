3b1f0addbeabae773399648915500363acf49bf7
s31233 <162576869+s31233@users.noreply.github.com>
2024-06-19 20:58:15 +0200

    TASK9(2)

--- Main.java
+++ Main.java
@@ -1,111 +1,53 @@
-import java.util.ArrayList;
-import java.util.List;
-//zadanie 1
+
+//zadanie 2
 public class Main {
     public static void main(String[] args) {
-
-        double radius = 3d;
-        Figure circle = new Circle(radius);
-
-        double baseSide = 10d;
-        double side = 5d;
-        double height = 8d;
-        Figure parallelogram = new Parallelogram(baseSide, side, height);
-
-        double sideA = 15d;
-        double sideB = 10;
-        Figure rectangle = new Rectangle(sideA, sideB);
-
-        List<Figure> figures = new ArrayList<>(3);
-        figures.add(circle);
-        figures.add(parallelogram);
-        figures.add(rectangle);
-
-        for (Figure figure : figures) {
-            System.out.println(figure.getType());
-            System.out.println("Area: " + figure.getArea() + " units.");
-            System.out.println("Perimeter: " + figure.getPerimeter() + " units.\n");
-        }
+        Cat mruczek = new Cat("Mruczek");
+        mruczek.start();
+        System.out.println(mruczek.getType());
+        mruczek.stop();
     }
 }
-//Klasa Circle implementuje interfejs Figure
-class Circle  implements Figure{
-    private double radius;
 
-    public Circle(double radius) {
-        this.radius = radius;
+//klasa abstrakcyjna Animal
+abstract class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract String getType();
+
+    public String getName() {
+        return name;
+    }
+}
+
+//Klasa Cat dziedzicząca klase Animal
+class Cat extends Animal implements Moveable {
+    public Cat(String name) {
+        super(name);
     }
 
     @Override
-    public double getPerimeter() {
-        return 2 * Math.PI * radius;
+    public void start() {
+        System.out.println("Cat starts!");
     }
 
     @Override
-    public double getArea() {
-        return Math.PI * radius * radius;
+    public void stop() {
+        System.out.println("Cat stopped!");
     }
 
     @Override
     public String getType() {
-        return "Circle";
+        return "Cat";
     }
 }
-//Klasa Rectangle implementuje interfejs Figure
-class Rectangle implements Figure {
-    private double sideA;
-    private double sideB;
 
-    public Rectangle(double sideA, double sideB) {
-        this.sideA = sideA;
-        this.sideB = sideB;
-    }
-
-    @Override
-    public double getPerimeter() {
-        return 2 * (sideA + sideB);
-    }
-
-    @Override
-    public double getArea() {
-        return sideA * sideB;
-    }
-
-    @Override
-    public String getType() {
-        return "Rectangle";
-    }
-}
-//Klasa Parallelogram implementuje interfejs Figure
-class Parallelogram implements Figure {
-    private double baseSide;
-    private double side;
-    private double height;
-
-    public Parallelogram(double baseSide, double side, double height) {
-        this.baseSide = baseSide;
-        this.side = side;
-        this.height = height;
-    }
-
-    @Override
-    public double getPerimeter() {
-        return 2 * (baseSide + side);
-    }
-
-    @Override
-    public double getArea() {
-        return baseSide * height;
-    }
-
-    @Override
-    public String getType() {
-        return "Parallelogram";
-    }
-}
-//Interfejs Figure
-interface Figure {
-    double getPerimeter();
-    double getArea();
-    String getType();
+//Interfejs Moveable
+interface Moveable {
+    void start();
+    void stop();
 }
