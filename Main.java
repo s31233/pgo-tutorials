93a38c4a8a2a144407b59d6f5d56938b40a037d7
s31233 <162576869+s31233@users.noreply.github.com>
2024-06-19 20:59:21 +0200

    TASK9(3)

--- Main.java
+++ Main.java
@@ -1,53 +1,75 @@
-
-//zadanie 2
+import java.util.Calendar;
+//Zadanie 3
 public class Main {
     public static void main(String[] args) {
-        Cat mruczek = new Cat("Mruczek");
-        mruczek.start();
-        System.out.println(mruczek.getType());
-        mruczek.stop();
+        Manager manager = new Manager("Jan", "Kowalski", 1990, 2010, "Pjatk", 2000d, 100d);
+        System.out.println(manager);
     }
 }
+//Abstrakcyjna klasa Person
+abstract class Person {
+    protected String firstName;
+    protected String lastName;
+    protected int birthdayYear;
 
-//klasa abstrakcyjna Animal
-abstract class Animal {
-    protected String name;
-
-    public Animal(String name) {
-        this.name = name;
+    public Person(String firstName, String lastName, int birthdayYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdayYear = birthdayYear;
     }
 
-    public abstract String getType();
-
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public abstract int getAge();
 }
+//Klasa Employee
+class Employee extends Person {
+    private int hireDate;
+    private String companyName;
+    private double salary;
 
-//Klasa Cat dziedzicząca klase Animal
-class Cat extends Animal implements Moveable {
-    public Cat(String name) {
-        super(name);
+    public Employee(String firstName, String lastName, int birthdayYear, int hireDate, String companyName, double salary) {
+        super(firstName, lastName, birthdayYear);
+        this.hireDate = hireDate;
+        this.companyName = companyName;
+        this.salary = salary;
+    }
+
+    public int getJobSeniority() {
+        return Calendar.getInstance().get(Calendar.YEAR) - hireDate;
+    }
+
+    public double getSalary() {
+        return salary;
     }
 
     @Override
-    public void start() {
-        System.out.println("Cat starts!");
-    }
-
-    @Override
-    public void stop() {
-        System.out.println("Cat stopped!");
-    }
-
-    @Override
-    public String getType() {
-        return "Cat";
+    public int getAge() {
+        return Calendar.getInstance().get(Calendar.YEAR) - birthdayYear;
     }
 }
+//Klasa Manager
+class Manager extends Employee {
+    private double bonus;
 
-//Interfejs Moveable
-interface Moveable {
-    void start();
-    void stop();
+    public Manager(String firstName, String lastName, int birthdayYear, int hireDate, String companyName, double salary, double bonus) {
+        super(firstName, lastName, birthdayYear, hireDate, companyName, salary);
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double getSalary() {
+        return super.getSalary() + bonus;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager with lastName " + getLastName() + " and age " + getAge() + " has salary " + getSalary();
+    }
 }
